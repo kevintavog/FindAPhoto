dfc16f596759009cfddb640066bc3f2677485904
kevintavog <kevin.tavog@zoho.com>
2016-11-18 08:54:59 -0800

    Fix an off-by-one for paging

--- findaphotoserver/content/src/app/providers/search-results.provider.ts
+++ findaphotoserver/content/src/app/providers/search-results.provider.ts
@@ -49,7 +49,7 @@ export class SearchResultsProvider {
                 }
 
                 let pageCount = this.searchResults.totalMatches / SearchResultsProvider.ItemsPerPage;
-                this.totalPages = Math.round(pageCount + (pageCount > Math.floor(pageCount) ? 1 : 0));
+                this.totalPages = Math.round(pageCount);
                 this.currentPage = Math.round(1 + (this.searchRequest.first / SearchResultsProvider.ItemsPerPage));
 
                 let dates = this.categoryDate();
