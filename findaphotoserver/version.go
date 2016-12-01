f1890dc467c8fb029af96d125bbfb073f1faa74d
kevintavog <kevin.tavog@zoho.com>
2016-11-30 20:08:03 -0800

    1.0.0 was released

--- findaphotoserver/version.go
+++ findaphotoserver/version.go
@@ -6,7 +6,7 @@ import (
 
 var majorVersion = 1
 var minorVersion = 0
-var buildVersion = 0
+var buildVersion = 1
 
 func versionString() string {
 	return fmt.Sprintf("%d.%d.%d", majorVersion, minorVersion, buildVersion)
